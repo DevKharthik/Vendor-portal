19a6d470b49a673485d875eea24d465ea2227c73
bala28102003 <balaprasanna28102003@gmail.com>
2025-07-20 00:07:21 +0530

    memo

--- sapClient/server.js
+++ sapClient/server.js
@@ -10,8 +10,8 @@ const vendorPoRoute = require('./vendorPoRoute');
 const vendorLoginRoute = require('./vendorLoginRoute');
 const vendorProfileRoute = require('./vendorProfileRoute');
 const vendorGrRoute = require('./vendorGsRoute');
-// const vendorGrRoute = require('./vendorGsRoute');
-const vendorInvoiceRoute=require('./vendorInvoiceRoute')
+const vendorInvoiceRoute=require('./vendorInvoiceRoute');
+const vendorMemoRoute=require('./vendorMemoRoute');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +26,8 @@ app.use('/api/vendor/rfq', rfqRoute);
 app.use('/api/vendor/po', vendorPoRoute );
 app.use('/api/vendor/gr', vendorGrRoute );
 app.use('/api/vendor/invoice',vendorInvoiceRoute)
+app.use('/api/vendor/memo',vendorMemoRoute)
+
 // Start server
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
